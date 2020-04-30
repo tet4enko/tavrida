f54c7cde82e1453964e117ef0b7ec7a1be8b7952
Dmitriy Tetchenko <tet4enko@yandex-team.ru>
2020-04-30 03:53:28 +0300

    юю

--- client/src/components/common/SuperFooter/SuperFooter.js
+++ client/src/components/common/SuperFooter/SuperFooter.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./SuperFooter.scss";
 import back from "./back.png";
 
-import logo from "./logo.png";
-import phone from "./phone.png";
-import mail from "./mail.png";
+import tel from "./Телефон.png";
+import car from "./Машина.png";
+import design from "./Дизайнер.png";
 
 import vk from "./vk.png";
 import inst from "./inst.png";
 import gl from "./gl.png";
+
+import mail from "./Почта.png";
+import time from "./Часы.png";
 
 class SuperFooter extends Component {
 	constructor(props) {
@@ -16,6 +19,63 @@ class SuperFooter extends Component {
 	}
 
 	render() {
+		const socialIconSize = "30px";
+		const iconSize = "45px";
+
+		const rabotniki = [
+			{
+				icon: tel,
+				position: "Менеджер по заказам",
+				tel1: (
+					<React.Fragment>
+						7 978 <b>079 09 79</b>
+					</React.Fragment>
+				),
+				tel2: "+79780790979",
+				side: "left"
+			},
+			{
+				icon: car,
+				position: "Логистика",
+				tel1: (
+					<React.Fragment>
+						7 978 <b>823 51 37</b>
+					</React.Fragment>
+				),
+				tel2: "+79788235137",
+				side: "center"
+			},
+			{
+				icon: design,
+				position: "Дизайнер",
+				tel1: (
+					<React.Fragment>
+						7 978 <b>590 51 56</b>
+					</React.Fragment>
+				),
+				tel2: "+79785905156",
+				side: "right"
+			}
+		];
+
+		const rabotnik = (icon, position, tel1, tel2) => {
+			return (
+				<div className="rabotnik">
+					<img src={icon} height={iconSize} />
+					<div className="info">
+						<div className="position">{position}</div>
+						<a
+							className="tel"
+							itemProp="telephone"
+							href={`tel:${tel2}`}
+						>
+							{tel1}
+						</a>
+					</div>
+				</div>
+			);
+		};
+
 		return (
 			<div className="SuperFooter">
 				<div className="main" width="1200px" height="560px">
@@ -23,107 +83,71 @@ class SuperFooter extends Component {
 						© 2015-2020 Рекламное агенство «ТАВРИДА МЕДИА» – реклама
 						в Симферополе и другие городах Крыма
 					</div>
-					<a href="/" className="logo">
-						<img
-							className="pic"
-							src={logo}
-							width="135px"
-							height="135px"
-						/>
-					</a>
-					<div className="left-col col">
-						<a className="col-link" href="vk.com">
-							УСЛУГИ
-						</a>
-						<a className="col-link" href="vk.com">
-							ЦЕНЫ
+
+					{rabotniki.map(item => {
+						return (
+							<div className={`col coltop ${item.side}`}>
+								{rabotnik(
+									item.icon,
+									item.position,
+									item.tel1,
+									item.tel2
+								)}
+							</div>
+						);
+					})}
+
+					<div className="col left colbottom mail">
+						<img src={mail} height={iconSize}></img>
+						<a target="_blank" href="mailto:tavrida.media@mail.ru">
+							tavrida.media@mail.ru
 						</a>
 					</div>
-					<div className="right-col col">
-						<a className="col-link" href="vk.com">
-							ПОРТФОЛИО
-						</a>
-						<a className="col-link" href="vk.com">
-							БЛОГ
-						</a>
+
+					<div className="col center colbottom time">
+						<img src={time} height={iconSize}></img>
+						<span className="text">10:00 - 19:00 / Пн-Пт</span>
 					</div>
-					<div className="contacts col">
-						<div className="top">
-							<div className="pics">
-								<img
-									src={phone}
-									width="55px"
-									height="55px"
-									className="pic"
-								/>
-								<img
-									src={mail}
-									width="35px"
-									height="35px"
-									className="pic"
-								/>
-							</div>
-							<div className="links">
-								<div className="tel-wrapper first">
-									<div className="label">
-										Менеджер по заказам
-									</div>
-									<a
-										className="tel"
-										itemProp="telephone"
-										href="tel:+79780790979 "
-									>
-										7 978 <b>079 09 79</b>
-									</a>
-								</div>
-								<div className="tel-wrapper second">
-									<div className="label">Логистика</div>
-									<a
-										className="tel"
-										itemProp="telephone"
-										href="tel:+79780790979 "
-									>
-										7 978 <b>823 51 37</b>
-									</a>
-								</div>
-								<div className="tel-wrapper thirth">
-									<div className="label">Дизайнер</div>
-									<a
-										className="tel"
-										itemProp="telephone"
-										href="tel:+79780790979 "
-									>
-										7 978 <b>590 51 56</b>
-									</a>
-								</div>
-								<a
-									className="email"
-									itemProp="email"
-									target="_blank"
-									href="mailto:tavrida.media@mail.ru"
-								>
-									tavrida.media@mail.ru
-								</a>
-							</div>
-						</div>
-						<div className="bottom" id="social-links">
+
+					<div className="col right colbottom callback">
+						<button className="btn">ПЕРЕЗВОНИТЬ ВАМ?</button>
+					</div>
+
+					<div className="social" id="social-links">
+						<span className="text">Следите за нами в:</span>
+						<div className="icons">
 							<a
+								className="icon"
 								itemProp="sameAs"
 								href="https://www.instagram.com/tavridamedia"
 							>
-								<img src={inst} width="25px" height="25px" />
+								<img
+									src={inst}
+									width={socialIconSize}
+									height={socialIconSize}
+								/>
 							</a>
 							<a
+								className="icon"
 								itemProp="sameAs"
 								href="https://vk.com/tavrida.media"
 							>
-								<img src={vk} width="25px" height="25px" />
+								<img
+									src={vk}
+									width={socialIconSize}
+									height={socialIconSize}
+								/>
 							</a>
 							<a
+								className="icon"
 								itemProp="sameAs"
 								href="https://vk.com/tavrida.media"
 							>
-								<img src={gl} width="25px" height="25px" />
+								<img
+									src={gl}
+									width={socialIconSize}
+									height={socialIconSize}
+								/>
 							</a>
 						</div>
 					</div>
